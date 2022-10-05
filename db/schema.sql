dedbee93d469228f225774689798f333e7976b1a
bobbeekethdy1025 <bobbeekethdy@gmail.com>
2022-10-04 19:49:48 -0500

    added and connected npms with server.js via require function

--- db/schema.sql
+++ db/schema.sql
@@ -3,13 +3,11 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
--- Department Table --
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
--- Role Table -- 
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
     title VARCHAR(30) NOT NULL,
@@ -20,7 +18,6 @@ CREATE TABLE role (
     ON DELETE SET NULL
 );
 
--- Employee Table -- 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
--- server.js
+++ server.js
@@ -1,4 +1,26 @@
+// This code was made with the help of my peers. Also had the reference of screenshots provided by the curriculum
 const express = require('express');
+const consoleTable = require('console.table');
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
-const consoleTable = require('console.table');
+
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'rootroot',
+        database: 'employee_db'
+    }
+);
+
+function init() {
+    welcomeScreen();
+    startApp();
+}
