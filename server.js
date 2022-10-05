755b67d79ab45409acffca2952f77b4f1f697c65
bobbeekethdy1025 <bobbeekethdy@gmail.com>
2022-10-04 20:32:37 -0500

    added inquirer prompts and some functions to server.js

--- server.js
+++ server.js
@@ -24,3 +24,56 @@ function init() {
     welcomeScreen();
     startApp();
 }
+
+const startApp = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'option',
+                message: 'Browse the catalog.',
+                choices: ['View all departments', 'View all employees', 'View all roles', 'Add a department', 'Add an employee', 'Add an role', 'Update an employee role', 'Exit']
+            }
+        ]).then((answers) => {
+            switch(answers.option) {
+                case 'View all departments':
+                    viewDeparts();
+                    break;
+                case 'View all employees':
+                    viewEmployees();
+                    break;
+                case 'View all roles':
+                    viewRoles();
+                    break;
+                case 'Add a department':
+                    addDepart();
+                    break;
+                case 'Add an employee':
+                    addEmployee();
+                    break;
+                case 'Add a role':
+                    addRole();
+                    break;
+                case 'Update an employee role':
+                    updateEmployee();
+                    break;
+                case 'Exit':
+                    exitPrompt();
+                    break;
+            }
+        });
+};
+function viewDeparts() {
+    db.query('SELECT * FROM department', function (err, res) {
+        if(err) throw err;
+        console.table('All Departments:', res);
+        startApp();
+    });
+};
+function viewRoles() {
+    db.query('SELECT role.id, role.title, department.name, role.salary FROM role JOIN department ON role.department_id = department.id', function (err, res) {
+        if(err) throw err;
+        console.table('All Roles:', res);
+        startApp();
+    });
+};
